fee58084220ab9751dfa1d177104bdc8a31ca3d7
Luiz Silveira <luiz_ssilveira@hotmail.com>
2018-05-02 17:18:01 -0300

    get id Solicitacao

--- public/js/req.js
+++ public/js/req.js
@@ -1,7 +1,10 @@
-var solicitacaoTr = document.createElement("tr");
 
+var listRequisicao = []
+var idSolicitacao
+
+var solicitacaoTr = document.createElement("tr");
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://localhost:3000/requisicoes/listarRequisicoes"); //tipo de requisição + end.
+xhr.open("GET", "https://raw.githubusercontent.com/LuizASSilveira/pi-almoxarifado/master/teste.json"); //tipo de requisição + end.
 xhr.addEventListener("load", function(){
     var sol = JSON.parse(xhr.responseText);
     sol.forEach(function(solicitacao) {
@@ -10,6 +13,34 @@ xhr.addEventListener("load", function(){
 })
 
 xhr.send();
+
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("click", function(event){  
+    idSolicitacao = event.target.parentNode.lastChild.textContent
+
+    if(!listRequisicao.includes(idSolicitacao)){
+        listRequisicao.push(idSolicitacao)    
+    }
+    else{
+        listRequisicao.pop(idSolicitacao)    
+    }
+    console.log(listRequisicao)
+    
+    //setTimeout(function(){
+    //    event.target.parentNode.remove(); //pega campo do duplo click e elimina o pai , fazendo assim apagar a linha
+    //},300);
+    
+});
+
+
+
+
+
+
+///////////////////////////
+
 function addSolicitacaoNaTabela(solicitacao){
     var solicitacaoTr = montaTr(solicitacao);
     var tabela = document.querySelector("#tabela-solicitacao");
@@ -19,10 +50,12 @@ function montaTr(solicitacao){
     var solicitacaoTr = document.createElement("tr");
     solicitacaoTr.classList.add("solicitacao");
  
-    solicitacaoTr.appendChild(montaTd(solicitacao.data,         "info-data"));
-    solicitacaoTr.appendChild(montaTd(solicitacao.descricao,    "info-descricao"));
-    solicitacaoTr.appendChild(montaTd(solicitacao.status,       "info-status"));
-    solicitacaoTr.appendChild(montaTd(solicitacao.usuario.nome,  "info-solicitante"));
+    solicitacaoTr.appendChild(montaTd(solicitacao.data,         "info-data"         ));
+    solicitacaoTr.appendChild(montaTd(solicitacao.descricao,    "info-descricao"    ));
+    solicitacaoTr.appendChild(montaTd(solicitacao.status,       "info-status"       ));
+    solicitacaoTr.appendChild(montaTd(solicitacao.usuario.nome, "info-solicitante"  ));
+    
+    solicitacaoTr.appendChild(montaTd(solicitacao.id,           "info-id"           ));
 
     return solicitacaoTr;
  }
